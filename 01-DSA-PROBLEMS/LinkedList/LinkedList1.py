71c2819cf0faaa0edf2f627ddd3024669810ee2d
Swapnil Gupta <swapnilgupta.xi@gmail.com>
2025-06-08 22:41:21 +0530

    linkedList-into

--- 01-DSA-PROBLEMS/LinkedList/LinkedList1.py
+++ 01-DSA-PROBLEMS/LinkedList/LinkedList1.py
@@ -9,8 +9,73 @@ class LinkedList:
         self.head = None
         
     def insert_at_beginning(self, data):
-        node = Node(data)
-        self.head = node if self.head is None else self.head
+        node = Node(data, self.head)
+        self.head = node 
+    
+    def print(self):
+        # itr is head node 
+        
+        itr= self.head 
+        llstr= ""
+        
+        while itr:
+            suffix = ""
+            if itr.next:
+                suffix = " --> "
+            llstr += str(itr.data) + suffix
+            itr = itr.next
+        print(llstr)
+        
+        if self.head is None:
+            print("Linked list is empty")
+            return
+        
+ 
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.head= Node(data, None )
+            return
+            
+        itr = self.head
+        while itr.next:
+            itr= itr.next
+            
+        itr.next= Node(data)
+        
+    def get_length(self):
+            count = 0
+            itr= self.head 
+            while itr:
+                count +=1
+                itr = itr.next
+            return count
+    
+    def insert_at(self, index, data):
+        itr= self.head
+        count=0
+        if index < 0 or index > self.get_length():
+            raise Exception("Invalid Index")
+
+        if index == 0:
+            self.insert_at_beginning(data)
+            return
+        
+        while itr:
+            count+=1
+            if count==index-1:
+                itr.next= Node(data, itr.next)
+                break
+            itr= itr.next
+
+        
         
 if __name__ == "__main__":
-    pass
+    root = LinkedList()
+    
+    root.insert_at_end(50)
+    root.insert_at_beginning(5)
+    root.insert_at_beginning(15)
+    root.insert_at_beginning(10)
+    root.insert_at(4, 20)
+    print(root.get_length())
+    root.print()
